2d09c4c04a6cc809c7bcb9d03df280172da58f84
Mikael Vanhemert <mike.vanhemert@defenseunicorns.com>
2023-06-08 17:09:14 -0600

    Small changes

--- testtranscirbe/main.py
+++ testtranscirbe/main.py
@@ -2,5 +2,6 @@ import openai
 openai.api_key = 'Free the models'
 openai.api_base = "https://leapfrogai.dd.bigbang.dev"
 
-audio_file= open("/Users/mvanhemert/Desktop/microphone-recording.webm", "rb")
+audio_file= open("/Users/mvanhemert/Desktop/test.webm", "rb")
 transcript = openai.Audio.translate("whisper-1", audio_file, language="en") # openai.Audio.transcribe("whisper-1", audio_file)
+print(transcript)
